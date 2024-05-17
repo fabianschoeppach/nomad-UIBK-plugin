b5fceba617c58159e6dffabfc808ec9e8204f83b
Fabian Schöppach <163308053+fabianschoeppach@users.noreply.github.com>
2024-05-17 15:37:23 +0000

    Initial commit for XRFschema and XRFreader

--- src/nomad_ubik_plugin/schema_packages/__init__.py
+++ src/nomad_ubik_plugin/schema_packages/__init__.py
@@ -7,11 +7,24 @@ class MySchemaPackageEntryPoint(SchemaPackageEntryPoint):
 
     def load(self):
         from nomad_ubik_plugin.schema_packages.mypackage import m_package
- 
+
         return m_package
 
 
 mypackage = MySchemaPackageEntryPoint(
     name='MyPackage',
     description='Schema package defined using the new plugin mechanism.',
+)
+
+class XRFSchemaPackageEntryPoint(SchemaPackageEntryPoint):
+    parameter: int = Field(0, description='Custom configuration parameter')
+
+    def load(self):
+        from nomad_ubik_plugin.schema_packages.XRFschema import m_package
+
+        return m_package
+
+xrfschema = XRFSchemaPackageEntryPoint(
+    name='XRFSchema',
+    description='XRF Schema package defined using the new plugin mechanism.',
 )
