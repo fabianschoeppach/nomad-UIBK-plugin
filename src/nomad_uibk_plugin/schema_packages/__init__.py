4c6ba21030ac0cb02c5c592fce200390c8b8b636
Fabian Schöppach <fabian.schoeppach@physik.hu-berlin.de>
2024-06-25 15:35:01 +0200

    Configure MicroCell Schema Entry Point in `schema_packages/__init__.py` and `pyproject.toml`

--- src/nomad_uibk_plugin/schema_packages/__init__.py
+++ src/nomad_uibk_plugin/schema_packages/__init__.py
@@ -25,15 +25,28 @@ mypackage = MySchemaPackageEntryPoint(
     description='Schema package defined using the new plugin mechanism.',
 )
 
-class XRFSchemaPackageEntryPoint(SchemaPackageEntryPoint):
-    parameter: int = Field(0, description='Custom configuration parameter')
 
+class XRFSchemaPackageEntryPoint(SchemaPackageEntryPoint):
     def load(self):
         from nomad_uibk_plugin.schema_packages.XRFschema import m_package
 
         return m_package
 
+
 xrfschema = XRFSchemaPackageEntryPoint(
     name='XRFSchema',
     description='XRF Schema package defined using the new plugin mechanism.',
+)
+
+
+class MicroCellSchemaPackageEntryPoint(SchemaPackageEntryPoint):
+    def load(self):
+        from nomad_uibk_plugin.schema_packages.microcellschema import m_package
+
+        return m_package
+
+
+microcellschema = MicroCellSchemaPackageEntryPoint(
+    name='MicroCellSchema',
+    description='MicroCell Schema package defined using the new plugin mechanism.',
 )
