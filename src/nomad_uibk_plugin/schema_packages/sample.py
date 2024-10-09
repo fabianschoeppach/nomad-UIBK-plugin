c374dccee5a5fdcfe9d3002b7df358af90422640
Fabian Schöppach <fabian.schoeppach@physik.hu-berlin.de>
2024-10-09 12:02:02 +0200

    Add UIBKSample class

---
+++ src/nomad_uibk_plugin/schema_packages/sample.py
@@ -0,0 +1,5 @@
+from nomad.datamodel.metainfo.basesections import CompositeSystem #CompositeSystemReference
+from nomad.datamodel.data import EntryData #ArchiveSection,
+
+class UIBKSample(CompositeSystem, EntryData):
+    pass
